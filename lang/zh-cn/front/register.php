3d3fa67b5d0bf55cecefa595d1548c055982c6ff
iszmxw <mail@54zm.com>
2025-07-22 01:49:59 +0800

    合并最新分支代码到当前分支-解决合并问题

--- lang/zh-cn/front/register.php
+++ lang/zh-cn/front/register.php
@@ -8,17 +8,9 @@
  */
 
 return [
-<<<<<<<< HEAD:lang/zh-hk/front/register.php
-    'have_account'     => '已有賬號？點擊登錄',
-    'register_submit'  => '立即註冊',
-    'register_success' => '註冊成功',
-    'register'         => '注册',
-    'register_text'    => '請在下麵表單中填寫注册資訊',
-========
     'have_account'     => '已有账号？点击登录',
     'register_submit'  => '立即注册',
     'register_success' => '注册成功',
     'register'         => '注册',
     'register_text'    => '请在下面表单中填写注册信息',
->>>>>>>> fix-article:lang/zh-cn/front/register.php
 ];
